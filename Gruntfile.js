0415bda3e9c8d9706978fc6fa6b5ad9b468bc9a1
Lucas Almeida <lucasecalmeida@gmail.com>
2020-10-01 15:58:23 -0300

    Atualizando processos de build e dependências

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,115 +1,218 @@
-const sass = require('node-sass');
-
 module.exports = function(grunt){
 
-	// Configuração do projeto
-	grunt.initConfig({
-		pkg:grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg:grunt.file.readJSON('package.json'),
 
-		// WATCH
-		watch:{
-			styles:{
-				files:[
-					'dynamic/scss/**/*.scss',
-					'dynamic/js/*.js'
-				],
-				tasks:['sass:dev','postcss','uglify']
-			}
-		},
+    watch:{
+      styles:{
+        files:[ 'source/styles/**/*.scss' ],
+        tasks:['sass:dev','postcss:dev'],
+        options: { livereload: true }
+      },
+      scripts:{
+        files:[ 'source/scripts/**/*.js' ],
+        tasks: ['uglify:dev', 'concat'],
+        options: { livereload: true }
+      },
+      files: {
+        files: [ 'source/images/**/*', 'source/fonts/**/*' ],
+        tasks:['copy:dev','cwebp:dev'],
+        options: { livereload: true }
+      },
+      static:{
+        files:[ '*.php', '**/*.php' ],
+        options: { livereload: true }
+      }
+    },
 
-		sass: {
-			dev: {
-				options: {
-						implementation: sass,
-				    style: 'compact'
-				},
-				files: {
-				    'static/css/main.css': 'dynamic/scss/main.scss'
-				}
-			},
-		},
+    sass: {
+      dev: {
+        options: {
+          style: 'compact'
+        },
+        files: {
+          'static/styles/main.min.css': 'source/styles/main.scss'
+        }
+      },
+      dist: {
+        options: {
+          style: 'minified'
+        },
+        files: {
+          'dist/static/styles/main.min.css': 'source/styles/main.scss'
+        }
+      },
+    },
 
     postcss: {
+      options: {
+        processors: [
+          require('pixrem')(),
+          require('autoprefixer')({browsers: 'last 2 versions'}),
+          require('cssnano')()
+        ]
+      },
+      dev: {
+        src: 'static/styles/main.min.css'
+      },
+      dist: {
+        src: 'dist/static/styles/main.min.css'
+      }
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          mangle:false,
+          beautify:true,
+        },
+        files: {
+          'static/scripts/main.min.js': ['source/scripts/main.js']
+        }
+      },
+      dist: {
+        options: {
+          mangle:true,
+          beautify:false,
+        },
+        files: {
+          'dist/static/scripts/main.min.js': ['source/scripts/main.js']
+        }
+      }
+    },
+
+    concat: {
 			options: {
-				processors: [
-					require('pixrem')(),
-					require('autoprefixer')({browsers: 'last 2 versions'}),
-					require('cssnano')()
-				]
+				separator: ';',
 			},
 			dev: {
-				src: 'static/css/main.css'
-			}
-		},
-
-		// UGLIFY
-		uglify: {
-			options: {
-				mangle: false,
-				beautify:false,
+				src: ['static/scripts/vendor/*.js', 'static/scripts/main.min.js'],
+				dest: 'static/scripts/main.min.js',
 			},
-			my_target: {
-				files: {
-				'static/js/main.js': ['dynamic/js/main.js']
-				}
-			}
-		},
+			dist: {
+				src: ['dist/static/scripts/vendor/*.js', 'dist/static/scripts/main.min.js'],
+				dest: 'dist/static/scripts/main.min.js',
+			},
+    },
 
-		// NOTIFY
-		notify_hooks: {
-			options: {
-				enabled: true,
-				max_jshint_notifications: 5,
-				title: "Grunt task",
-				success: true,
-				duration: 3
-			}
-		},
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: './'
+        }
+      }
+    },
 
     clean: {
-			dist: {
-				src: ['dist']
-			}
-		},
+      dev: {
+        src: ['static']
+      },
+      dist: {
+        src: ['dist']
+      }
+    },
 
-		copy: {
-			dist: {
-				files: [
-					{
-						expand: true,
-						cwd: 'static/',
-						src: '**/*',
-						dest: 'dist/static/'
-					},
-					{
-						expand: true,
-						dot: true,
-						cwd: './',
-						dest: 'dist/',
-						src: [
-							'favicon.png',
-							'index.html'
-						]
-					}
-				]
-            }
+    cwebp: {
+      dev: {
+        options: {
+          q: 80,
         },
+        files: [{
+          expand: true,
+          cwd: 'source/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'static/images/'
+        }]
+      },
+      dist: {
+        options: {
+          q: 80,
+        },
+        files: [{
+          expand: true,
+          cwd: 'source/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/static/images/'
+        }]
+      }
+    },
 
-	});
+    copy: {
+      dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'source/scripts/vendor/',
+            src: '**/*',
+            dest: 'static/scripts/vendor/'
+          },
+          {
+            expand: true,
+            cwd: 'source/fonts/',
+            src: '**/*',
+            dest: 'static/fonts/'
+          }
+        ]
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'source/scripts/vendor/',
+            src: '**/*',
+            dest: 'dist/static/scripts/vendor/'
+          },
+          {
+            expand: true,
+            cwd: 'source/fonts/',
+            src: '**/*',
+            dest: 'dist/static/fonts/'
+          },
+          {
+            expand: true,
+            dot: true,
+            cwd: './',
+            dest: 'dist/',
+            src: [
+              'favicon.png',
+              'index.html'
+            ]
+          }
+        ]
+      }
+    },
 
-	// Carrega os plugins
-	grunt.loadNpmTasks('grunt-notify');
-  	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-postcss');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+    notify_hooks: {
+      options: {
+        enabled: true,
+        max_jshint_notifications: 5,
+        title: 'Compilado',
+        success: true,
+        duration: 3
+      }
+    },
 
-	grunt.task.run('notify_hooks');
+  });
 
-	// Tarefas padrão
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('compile', ['sass:dev','uglify','clean','copy','postcss']);
+  // Deps
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-cwebp');
+
+  // Notifications
+  grunt.task.run('notify_hooks');
+
+  // Tasks
+  grunt.registerTask('dev', ['clean:dev','copy:dev','cwebp:dev','sass:dev','postcss:dev','uglify:dev', 'concat','connect','watch']);
+  grunt.registerTask('build', ['clean:dev','copy:dev','cwebp:dev','sass:dev','postcss:dev','uglify:dev', 'concat']);
+  grunt.registerTask('deploy', ['clean:dist','copy:dist','cwebp:dist','sass:dist','postcss:dist','uglify:dist', 'concat']);
 
 };
