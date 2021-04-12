5443a31f44f67232ae6ee1df6686bf7ee100e364
Lucas Almeida <lucasecalmeida@gmail.com>
2021-04-12 16:10:47 -0300

    Setting up Github Actions

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $1 == 'prod' ] ; then
-    rsync -avzPp ./dist/ hostgator:/home/lucas079/lucasalmeida.dev
+    rsync -avzPp ./dist/ hostgator:/home2/lucas079/lucasalmeida.dev
     rsync -avzPp ./dist/ hostgator:/home/lucas079/public_html
     exit;
 fi
